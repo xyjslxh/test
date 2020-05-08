20c75a44b74fe2f432b5994bec7eb0ba8f873d17
xyjslxh <xyjslxh@163.com>
2020-05-08 20:15:21 +0800

    1

--- src/com/test/CreateThreadDemo.java
+++ src/com/test/CreateThreadDemo.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import sun.misc.Unsafe;
 
 public class CreateThreadDemo {
 
